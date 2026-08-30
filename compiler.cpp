ce78ecb78b4f6444af34cf399d917a47ffae67e8
Miraç Salih <isletmekanal40@gmail.com>
2026-08-30 20:31:46 +0300

    Refactor token handling with TypeToken enum

--- compiler.cpp
+++ compiler.cpp
@@ -53,8 +53,17 @@ static void return_error(const std::string& error_information, const int returns
     spesifik_ret = spesifik_returns;
 }
 
+typedef enum TypeToken {
+    unknow = 0,
+    keyword = 1,
+    varname = 2,
+    number = 3,
+    operato = 4,
+    definite = 5
+} TypeToken;
+
 typedef struct Token {
-    std::string type;
+    TypeToken typetoken;
     std::string value;
 } Token;
 
@@ -70,16 +79,33 @@ static int get_number(Token token) {
     }
 }
 
+static std::string get_snumber(Token token) {
+    try {
+        return std::to_string(stoi(token.value, 0, 10));
+    } catch (std::invalid_argument) {
+        return "";
+    }
+}
+
+static std::string type(Token token) {
+    if (token.typetoken == TypeToken::unknow) return "UNKNOW";
+    else if (token.typetoken == TypeToken::keyword) return "KEYWORD";
+    else if (token.typetoken == TypeToken::varname) return "VARNAME";
+    else if (token.typetoken == TypeToken::operato) return "OPERATOR";
+    else if (token.typetoken == TypeToken::definite) return "DEFINITE";
+    else return "";
+}
+
 static std::string tokenize() {
     if (tokens.empty()) return "";
     
-    if (tokens.size() == 1 && tokens[0].type == "KEYWORD" && tokens[0].value == "endif") {
+    if (tokens.size() == 1 && tokens[0].typetoken == TypeToken::keyword && tokens[0].value == "endif") {
         if (equal.empty()) return_error("Koşul sağlanmamış!", -18, "KOŞUL::KOŞUL_SAĞLANMAMIŞ");
         else equal.pop_back();
         return "";
     }
 
-    if (tokens.size() == 1 && tokens[0].type == "KEYWORD" && tokens[0].value == "else") {
+    if (tokens.size() == 1 && tokens[0].typetoken == TypeToken::keyword && tokens[0].value == "else") {
         if (equal.empty()) return_error("Else eşleşmiyor!", -19, "KOŞUL::ELSE_HATASI");
         else equal.back() = !equal.back();
         return "";
@@ -87,14 +113,14 @@ static std::string tokenize() {
     
     if (!equal.empty() && !equal.back()) return "";
 
-    if (tokens[0].type == "VARNAME" && tokens.size() == 1) {
+    if (tokens[0].typetoken == TypeToken::varname && tokens.size() == 1) {
         return tokens[0].value + " = " + var[tokens[0].value].value;
     }
 
-    else if (tokens[0].type == "KEYWORD" && tokens[0].value == "print") {
+    else if (tokens[0].typetoken == TypeToken::keyword && tokens[0].value == "print") {
         tokens.erase(tokens.begin());
         for (const auto& i : tokens) {
-            if (i.type == "VARNAME") std::cout << var[i.value].value << " ";
+            if (i.typetoken == TypeToken::varname) std::cout << var[i.value].value << " ";
             else std::cout << i.value << " ";
         }
         std::cout << std::endl;
@@ -102,66 +128,66 @@ static std::string tokenize() {
     }
 
     else if (tokens.size() == 2) {
-        if (tokens[0].type == "KEYWORD") {
+        if (tokens[0].typetoken == TypeToken::keyword) {
             if (tokens[0].value == "var") {
-                if (tokens[1].type == "UNKNOW") var[tokens[1].value] = {.type = "VARNAME", .value = "n"};
-                else if (tokens[1].type == "VARNAME") return_error("Değişken zaten tanımlanmış!", -8, "DEĞİŞKEN::ZATEN_TANIMLANMIŞ");
-                else if (tokens[1].type == "NUMBER") return_error("Değişken ismi sayısal olamaz!", -10, "DEĞİŞKEN::SAYISAL_OLAMAZ");
+                if (tokens[1].typetoken == TypeToken::unknow) var[tokens[1].value] = {.typetoken = TypeToken::varname, .value = "n"};
+                else if (tokens[1].typetoken == TypeToken::varname) return_error("Değişken zaten tanımlanmış!", -8, "DEĞİŞKEN::ZATEN_TANIMLANMIŞ");
+                else if (tokens[1].typetoken == TypeToken::number) return_error("Değişken ismi sayısal olamaz!", -10, "DEĞİŞKEN::SAYISAL_OLAMAZ");
             }
             
             else if (tokens[0].value == "del") {
-                if (tokens[1].type == "VARNAME") var.erase(tokens[1].value);
-                else if (tokens[1].type == "UNKNOW") return_error("Tanımsız bir değişken silinemez!", -9, "DEĞİŞKEN::TANIMLANMAMIŞ");
-                else if (tokens[1].type == "NUMBER") return_error("Değişken ismi sayısal olamaz!", -10, "DEĞİŞKEN::SAYISAL_OLAMAZ");
+                if (tokens[1].typetoken == TypeToken::varname) var.erase(tokens[1].value);
+                else if (tokens[1].typetoken == TypeToken::unknow) return_error("Tanımsız bir değişken silinemez!", -9, "DEĞİŞKEN::TANIMLANMAMIŞ");
+                else if (tokens[1].typetoken == TypeToken::number) return_error("Değişken ismi sayısal olamaz!", -10, "DEĞİŞKEN::SAYISAL_OLAMAZ");
             }
         }
     }
     
     else if (tokens.size() == 3) {
-        if (tokens[0].type == "VARNAME") {
-            if (tokens[1].type == "OPERATOR") {
+        if (tokens[0].typetoken == TypeToken::varname) {
+            if (tokens[1].typetoken == TypeToken::operato) {
                 if (tokens[1].value == "=") {
-                    if (tokens[2].type == "UNKNOW") return_error("Tanımsız bir şey...", -11, "TANIM::TANIMSIZ");
-                    else if (tokens[2].type == "NUMBER") var[tokens[0].value] = {.type = "NUMBER", .value = std::to_string(get_number(tokens[2]))};
-                    else if (tokens[2].type == "VARNAME") var[tokens[0].value] = {.type = "NUMBER", .value = std::to_string(get_number(var[tokens[2].value]))};
+                    if (tokens[2].typetoken == TypeToken::unknow) return_error("Tanımsız bir şey...", -11, "TANIM::TANIMSIZ");
+                    else if (tokens[2].typetoken == TypeToken::number) var[tokens[0].value] = {.typetoken = TypeToken::number, .value = get_snumber(tokens[2])};
+                    else if (tokens[2].typetoken == TypeToken::varname) var[tokens[0].value] = {.typetoken = TypeToken::number, .value = get_snumber(var[tokens[2].value])};
                 }
 
                 else if (tokens[1].value == "+") {
-                    if (tokens[2].type == "UNKNOW") return_error("Tanımsız bir şey...", -11, "TANIM::TANIMSIZ");
-                    else if (tokens[2].type == "NUMBER") var[tokens[0].value] = {.type = "NUMBER", .value = std::to_string(get_number(var[tokens[0].value]) + get_number(tokens[2]))};
-                    else if (tokens[2].type == "VARNAME") var[tokens[0].value] = {.type = "NUMBER", .value = std::to_string(get_number(var[tokens[0].value]) + get_number(var[tokens[2].value]))};
+                    if (tokens[2].typetoken == TypeToken::unknow) return_error("Tanımsız bir şey...", -11, "TANIM::TANIMSIZ");
+                    else if (tokens[2].typetoken == TypeToken::number) var[tokens[0].value] = {.typetoken = TypeToken::number, .value = std::to_string(get_number(var[tokens[0].value]) + get_number(tokens[2]))};
+                    else if (tokens[2].typetoken == TypeToken::varname) var[tokens[0].value] = {.typetoken = TypeToken::number, .value = std::to_string(get_number(var[tokens[0].value]) + get_number(var[tokens[2].value]))};
                 }
 
                 else if (tokens[1].value == "-") {
-                    if (tokens[2].type == "UNKNOW") return_error("Tanımsız bir şey...", -11, "TANIM::TANIMSIZ");
-                    else if (tokens[2].type == "NUMBER") var[tokens[0].value] = {.type = "NUMBER", .value = std::to_string(get_number(var[tokens[0].value]) - get_number(tokens[2]))};
-                    else if (tokens[2].type == "VARNAME") var[tokens[0].value] = {.type = "NUMBER", .value = std::to_string(get_number(var[tokens[0].value]) - get_number(var[tokens[2].value]))};
+                    if (tokens[2].typetoken == TypeToken::unknow) return_error("Tanımsız bir şey...", -11, "TANIM::TANIMSIZ");
+                    else if (tokens[2].typetoken == TypeToken::number) var[tokens[0].value] = {.typetoken = TypeToken::number, .value = std::to_string(get_number(var[tokens[0].value]) - get_number(tokens[2]))};
+                    else if (tokens[2].typetoken == TypeToken::varname) var[tokens[0].value] = {.typetoken = TypeToken::number, .value = std::to_string(get_number(var[tokens[0].value]) - get_number(var[tokens[2].value]))};
                 }
 
                 else if (tokens[1].value == "/") {
-                    if (tokens[2].type == "UNKNOW") return_error("Tanımsız bir şey...", -11, "TANIM::TANIMSIZ");
-                    else if (tokens[2].type == "NUMBER") var[tokens[0].value] = {.type = "NUMBER", .value = std::to_string(get_number(var[tokens[0].value]) / get_number(tokens[2]))};
-                    else if (tokens[2].type == "VARNAME") var[tokens[0].value] = {.type = "NUMBER", .value = std::to_string(get_number(var[tokens[0].value]) / get_number(var[tokens[2].value]))};
+                    if (tokens[2].typetoken == TypeToken::unknow) return_error("Tanımsız bir şey...", -11, "TANIM::TANIMSIZ");
+                    else if (tokens[2].typetoken == TypeToken::number) var[tokens[0].value] = {.typetoken = TypeToken::number, .value = std::to_string(get_number(var[tokens[0].value]) / get_number(tokens[2]))};
+                    else if (tokens[2].typetoken == TypeToken::varname) var[tokens[0].value] = {.typetoken = TypeToken::number, .value = std::to_string(get_number(var[tokens[0].value]) / get_number(var[tokens[2].value]))};
                 }
 
                 else if (tokens[1].value == "*") {
-                    if (tokens[2].type == "UNKNOW") return_error("Tanımsız bir şey...", -11, "TANIM::TANIMSIZ");
-                    else if (tokens[2].type == "NUMBER") var[tokens[0].value] = {.type = "NUMBER", .value = std::to_string(get_number(var[tokens[0].value]) * get_number(tokens[2]))};
-                    else if (tokens[2].type == "VARNAME") var[tokens[0].value] = {.type = "NUMBER", .value = std::to_string(get_number(var[tokens[0].value]) * get_number(var[tokens[2].value]))};
+                    if (tokens[2].typetoken == TypeToken::unknow) return_error("Tanımsız bir şey...", -11, "TANIM::TANIMSIZ");
+                    else if (tokens[2].typetoken == TypeToken::number) var[tokens[0].value] = {.typetoken = TypeToken::number, .value = std::to_string(get_number(var[tokens[0].value]) * get_number(tokens[2]))};
+                    else if (tokens[2].typetoken == TypeToken::varname) var[tokens[0].value] = {.typetoken = TypeToken::number, .value = std::to_string(get_number(var[tokens[0].value]) * get_number(var[tokens[2].value]))};
                 }
             }
         }
     }
 
     else if (tokens.size() == 4) {
-        if (tokens[0].type == "KEYWORD") {
+        if (tokens[0].typetoken == TypeToken::keyword) {
             if (tokens[0].value == "if") {
                 tokens.erase(tokens.begin());
                 for (size_t size = 0; size < tokens.size(); size++) {
-                    if (tokens[size].type == "VARNAME") {
+                    if (tokens[size].typetoken == TypeToken::varname) {
                         int deger = get_number(var[tokens[size].value]);
                         tokens[size].value = std::to_string(deger);
-                        tokens[size].type = "NUMBER";
+                        tokens[size].typetoken = TypeToken::number;
                     }
                 }
 
@@ -226,28 +252,28 @@ int main(const int argc, const char **argv) {
         for (const auto& [isim, veri] : var) {
             if (token == isim) {
                 finded = true;
-                tokens.push_back({.type = "VARNAME", .value = isim});
+                tokens.push_back({.typetoken = TypeToken::varname, .value = isim});
                 break;
             }
         }
 
         if (finded) {}
-        else if (token == "variable" || token == "var") tokens.push_back({.type = "KEYWORD", .value = "var"});
-        else if (token == "delete" || token == "del") tokens.push_back({.type = "KEYWORD", .value = "del"});
-        else if (token == "print") tokens.push_back({.type = "KEYWORD", .value = "print"});
+        else if (token == "variable" || token == "var") tokens.push_back({.typetoken = TypeToken::keyword, .value = "var"});
+        else if (token == "delete" || token == "del") tokens.push_back({.typetoken = TypeToken::keyword, .value = "del"});
+        else if (token == "print") tokens.push_back({.typetoken = TypeToken::keyword, .value = "print"});
 
-        else if (token == "if") tokens.push_back({.type = "KEYWORD", .value = "if"});
-        else if (token == "else") tokens.push_back({.type = "KEYWORD", .value = "else"});
-        else if (token == "endif") tokens.push_back({.type = "KEYWORD", .value = "endif"});
+        else if (token == "if") tokens.push_back({.typetoken = TypeToken::keyword, .value = "if"});
+        else if (token == "else") tokens.push_back({.typetoken = TypeToken::keyword, .value = "else"});
+        else if (token == "endif") tokens.push_back({.typetoken = TypeToken::keyword, .value = "endif"});
 
-        else if (token == "=") tokens.push_back({.type = "OPERATOR", .value = "="});
-        else if (token == "==") tokens.push_back({.type = "OPERATOR", .value = "=="});
-        else if (token == "+") tokens.push_back({.type = "OPERATOR", .value = "+"});
-        else if (token == "-") tokens.push_back({.type = "OPERATOR", .value = "-"});
-        else if (token == "/") tokens.push_back({.type = "OPERATOR", .value = "/"});
-        else if (token == "*") tokens.push_back({.type = "OPERATOR", .value = "*"});
+        else if (token == "=") tokens.push_back({.typetoken = TypeToken::operato, .value = "="});
+        else if (token == "==") tokens.push_back({.typetoken = TypeToken::operato, .value = "=="});
+        else if (token == "+") tokens.push_back({.typetoken = TypeToken::operato, .value = "+"});
+        else if (token == "-") tokens.push_back({.typetoken = TypeToken::operato, .value = "-"});
+        else if (token == "/") tokens.push_back({.typetoken = TypeToken::operato, .value = "/"});
+        else if (token == "*") tokens.push_back({.typetoken = TypeToken::operato, .value = "*"});
 
-        else if (token.front() == '#') tokens.push_back({.type = "DEFINITE", .value = token.substr(1)});
+        else if (token.front() == '#') tokens.push_back({.typetoken = TypeToken::definite, .value = token.substr(1)});
 
         else if (token.substr(0, 2) == "0x") {
             token.erase(0, 2);
@@ -284,7 +310,7 @@ int main(const int argc, const char **argv) {
                 continue;
             }
 
-            tokens.push_back({.type = "NUMBER", .value = std::to_string(stoi(token, 0, 16))});
+            tokens.push_back({.typetoken = TypeToken::number, .value = std::to_string(stoi(token, 0, 16))});
         }
 
         else if (token.front() == '1' ||
@@ -317,7 +343,7 @@ int main(const int argc, const char **argv) {
                 continue;
             }
             
-            tokens.push_back({.type = "NUMBER", .value = std::to_string(stoi(token, 0, 10))});
+            tokens.push_back({.typetoken = TypeToken::number, .value = std::to_string(stoi(token, 0, 10))});
         }
 
         else if (token.front() == '0') {
@@ -341,7 +367,7 @@ int main(const int argc, const char **argv) {
                 continue;
             }
 
-            tokens.push_back({.type = "NUMBER", .value = std::to_string(stoi(token, 0, 8))});
+            tokens.push_back({.typetoken = TypeToken::number, .value = std::to_string(stoi(token, 0, 8))});
         }
 
         else if (token.substr(0, 2) == "0b") {
@@ -359,12 +385,12 @@ int main(const int argc, const char **argv) {
                 continue;
             }
 
-            tokens.push_back({.type = "NUMBER", .value = std::to_string(stoi(token, 0, 2))});
+            tokens.push_back({.typetoken = TypeToken::number, .value = std::to_string(stoi(token, 0, 2))});
         }
 
-        else tokens.push_back({.type = "UNKNOW", .value = token});
+        else tokens.push_back({.typetoken = TypeToken::unknow, .value = token});
 
-        output << "İfade: " << ifade_no << " " << tokens[size].type << " | " << tokens[size].value << std::endl;
+        output << "İfade: " << ifade_no << " " << type(tokens[size]) << " | " << tokens[size].value << std::endl;
         size++;
     }
 
